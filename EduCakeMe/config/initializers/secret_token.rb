588184ffb8be814888e0ed6692fdb1d181c944af
Chintan Parikh <parikhcv@gmail.com>
2013-03-01 22:13:49 -0500

    Initial commit

---
+++ EduCakeMe/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EduCakeMe::Application.config.secret_token = '11721312e4ab4b0fdcc9e2b1b862182db25406dc0ee974a1a753d8780dedd291bbd7c78be685bca79b528e2fe0b51d6de116022b7e4634fd5ca540bc9d1055b5'
