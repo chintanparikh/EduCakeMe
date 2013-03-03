274e7f64879c5e0afb7bd88fbc52fd888fee7f51
chintanparikh <parikhcv@gmail.com>
2013-03-03 15:33:52 -0500

    Added cool js shit

---
+++ EduCakeMe/app/assets/javascripts/cakes.js
@@ -0,0 +1,10 @@
+$('document').ready(function(){
+	$('#content-box-1').click(function()
+	{
+		$('#content-box-1 .check').css({opacity: 1});
+	})
+	$('#content-box-2').click(function()
+	{
+		$('#content-box-2 .check').css({opacity: 1});
+	})
+});
